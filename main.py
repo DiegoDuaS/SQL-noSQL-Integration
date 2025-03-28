ed49f07e2360e762917f6cf2e40719a1033c1185
DiegoDuaS <diego.slowing@gmail.com>
2025-03-27 19:44:56 -0600

    feat: added gitignore

---
+++ main.py
@@ -0,0 +1,9 @@
+# Laboratorio 7 - Base de Datos
+# Fabiola Contreras
+# Diego Duarte
+# Maria Jose Villafuerte
+
+from sqlalchemy import create_engine
+from pymongo import MongoClient
+import pandas as pd
+
