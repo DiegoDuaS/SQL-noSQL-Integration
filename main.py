8cc4dfb22ec10fe285f7f7328b92b44f96aae60c
DiegoDuaS <diego.slowing@gmail.com>
2025-03-27 19:48:51 -0600

    feat: added mongodb connection

--- main.py
+++ main.py
@@ -6,4 +6,18 @@
 from sqlalchemy import create_engine
 from pymongo import MongoClient
 import pandas as pd
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Leer la URI desde las variables de entorno
+MONGO_URI = os.getenv("MONGO_URI")
+
+# Conectar a MongoDB Atlas
+client = MongoClient(MONGO_URI)
+db = client["Laboratorio7"]
+
+# Verificar conexión
+print(db.list_collection_names())  # Ver las colecciones disponibles
 
